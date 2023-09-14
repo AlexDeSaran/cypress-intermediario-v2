c187a9f10f9ff094fed3ef8dd84a03eabce0ceae
Alecio L. Medeiros <Alexdesaran@outlook.com>
2023-09-14 14:17:09 -0300

    Lessons 7, 8 and 9

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,7 +1,9 @@
 Cypress.Commands.add('login', (
+
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
   { cacheSession = true } = {},
+
 ) => {
   const login = () => {
     cy.visit('/users/sign_in')
@@ -33,6 +35,7 @@ Cypress.Commands.add('login', (
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
   })
+
   Cypress.Commands.add('gui_createProject', project => {
     cy.visit('/projects/new')
   
@@ -41,10 +44,22 @@ Cypress.Commands.add('login', (
     cy.get('.qa-initialize-with-readme-checkbox').check()//. identifica como classe o seletor
     cy.contains('Create project').click()
   })
+
   Cypress.Commands.add('gui_createIssue', issue => {
     cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
   
     cy.get('.qa-issuable-form-title').type(issue.title)
     cy.get('.qa-issuable-form-description').type(issue.description)
     cy.contains('Submit issue').click()
+  })
+
+  Cypress.Commands.add('gui_setLabelOnIssue', label => {
+    cy.get('.qa-edit-link-labels').click()
+    cy.contains(label.name).click()
+    cy.get('body').click()
+  })
+
+  Cypress.Commands.add('gui_setMilestoneOnIssue', milestone => {
+    cy.get('.block.milestone .edit-link').click()
+    cy.contains(milestone.title).click()
   })
